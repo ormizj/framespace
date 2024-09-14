24a7be4a7a43369ce67f3d94f0b1713d54c819f8
spiderpig60 <spiderpig60@gmail.com>
2024-09-14 18:03:55 +0300

    interact.js wip

--- components/grid-elements.vue
+++ components/grid-elements.vue
@@ -3,39 +3,74 @@
 import interact from 'interactjs'
 
 onMounted(() => {
-    var element = document.querySelector('.grid-snap')
-    var x = 0; var y = 0
+    const resize = (className: string) => {
+        var element: HTMLElement = document.querySelector(`.${className}`)!
+        var x = 0; var y = 0
 
-    interact(element)
-        .draggable({
-            modifiers: [
-                interact.modifiers.snap({
-                    targets: [
-                        interact.snappers.grid({ x: 30, y: 30 })
-                    ],
-                    range: Infinity,
-                    relativePoints: [{ x: 0, y: 0 }]
-                }),
-                interact.modifiers.restrict({
-                    restriction: element.parentNode,
-                    elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-                    endOnly: true
-                })
-            ],
-        })
-        .on('dragmove', function (event) {
-            x += event.dx
-            y += event.dy
+        interact(element)
+            .draggable({
+                modifiers: [
+                    interact.modifiers.snap({
+                        targets: [
+                            interact.snappers.grid({ x: 10, y: 10 })
+                        ],
+                        range: Infinity,
+                        relativePoints: [{ x: 0, y: 0 }]
+                    }),
+                    interact.modifiers.restrict({
+                        restriction: element.parentNode,
+                        elementRect: {
+                            top: 0,
+                            left: 0,
+                            bottom: 1,
+                            right: 1
+                        },
+                        endOnly: true
+                    })
+                ],
+            })
+            .on('dragmove', function (event) {
+                x += event.dx
+                y += event.dy
 
-            event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-        })
+                event.target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+            }).resizable({
+                edges: {
+                    bottom: true,
+                    left: true,
+                    right: true,
+                    top: true,
+                },
+                listeners: {
+                    move: function (event) {
+                        let { x, y } = event.target.dataset
+
+                        x = (parseFloat(x) || 0) + event.deltaRect.left
+                        y = (parseFloat(y) || 0) + event.deltaRect.top
+
+                        Object.assign(event.target.style, {
+                            width: `${event.rect.width}px`,
+                            height: `${event.rect.height}px`,
+                            transform: `translate(${x}px, ${y}px)`
+                        })
+
+                        Object.assign(event.target.dataset, { x, y })
+                    }
+                }
+            })
+    }
+
+    resize('block1');
+    resize('block2')
+    resize('block3')
 })
 </script>
 
 <template>
     <div class="grid-elements">
-        <div class="grid-snap">Block1</div>
-
+        <div class="grid-snap block1">Block1</div>
+        <div class="grid-snap block2">Block2</div>
+        <div class="grid-snap block3">Block3</div>
     </div>
 </template>
 
@@ -51,6 +86,6 @@ onMounted(() => {
 .grid-snap {
     background-color: #29e;
     touch-action: none;
-    grid-area: 11/11/15/15;
+    grid-area: 11/11/50/50;
 }
 </style>
