fd1b42e087d2e95762cfee4c84d2d0917a92342f
spiderpig60 <spiderpig60@gmail.com>
2024-09-14 13:33:10 +0300

    layout wip

---
+++ components/UI/nuxt-button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button class="nuxt-button">
+        <slot />
+    </button>
+</template>
+
+<style scoped></style>
---
+++ components/frame-space.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const isOpen = ref(false);
+</script>
+
+
+<template>
+  <div class="frame-space" :class="`${isOpen ? 'open' : 'close'}`">
+    <div class="top-tab">
+      <button class="top-tab-button" @click="isOpen = true">Open Frames</button>
+    </div>
+    <div class="content"></div>
+  </div>
+</template>
+
+<style scoped>
+.frame-space {
+  --tab-height: 2rem;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--background-color);
+
+  &.open {
+    top: calc(var(--tab-height) * -1);
+  }
+
+  &.close {
+    top: calc(100dvh - var(--tab-height));
+  }
+
+  .content {
+    width: 100dvw;
+    height: 100dvh;
+  }
+
+  .top-tab {
+    height: var(--tab-height);
+    pointer-events: none;
+
+    .top-tab-button {
+      pointer-events: auto;
+      height: 100%;
+    }
+  }
+}
+</style>
