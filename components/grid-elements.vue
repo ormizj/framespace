00108814c7af16933146bd95f564b99c4a3db1a2
spiderpig60 <spiderpig60@gmail.com>
2024-09-15 18:56:23 +0300

    wip

--- components/grid-elements.vue
+++ components/grid-elements.vue
@@ -3,33 +3,57 @@
 const link = "https://www.calculatorsoup.com/calculators/math/percentage.php"
 const editModel = defineModel('edit', { default: false });
 const gridElements = ref<HTMLDivElement | null>(null);
+const yGrid = ref(10);
+const xGrid = ref(10);
+
 
 onMounted(() => {
-
+    for (const element of gridElements.value!.children as unknown as HTMLElement[]) {
+        element.addEventListener('dragend', (e) => {
+            console.log(e);
+        });
+    }
 });
 </script>
 
 <template>
     <div class="grid-elements" ref="gridElements" :class="{ edit: editModel }">
-        <div class="grid-snap block1">
-            <iframe :src="link" class="iframe" />
-        </div>
-        <div class="grid-snap block2">
-            <iframe :src="link" class="iframe" />
-        </div>
-        <div class="grid-snap block3">
-            <iframe :src="link" class="iframe" />
+        <div v-for="i in yGrid" class="y-grid">
+            <div v-for="j in xGrid" class="x-grid">
+
+                <template v-if="i === 1 && j === 3">
+                    <div class="grid-snap block3" :draggable="editModel">
+                        <iframe :src="link" class="iframe" />
+                    </div>
+                </template>
+
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.y-grid {
+    display: flex;
+    height: 10vh;
+    width: 100%;
+}
+
+.x-grid {
+    height: 100%;
+    width: 100%;
+
+    outline: 1px solid whitesmoke;
+}
+
 .grid-elements {
     width: 100%;
     min-height: 100dvh;
-    display: grid;
-    grid-template-columns: repeat(10, 1fr);
-    grid-template-rows: repeat(auto-fill, 10vh);
+
+    .grid-snap {
+        width: 100%;
+        height: 100%;
+    }
 
     .iframe {
         border: unset;
@@ -37,6 +61,8 @@ onMounted(() => {
         height: 100%;
     }
 }
+
+
 
 .grid-elements.edit {
     .iframe {
