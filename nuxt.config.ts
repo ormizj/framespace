4c04ae6c9f33e045c2a5bc019a3695572b38960a
spiderpig60 <spiderpig60@gmail.com>
2024-09-21 02:09:25 +0300

    base configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	modules: ['@nuxthub/core','@pinia/nuxt'],
+
+	hub: {
+		database: true,
+	},
+
+	devtools: { enabled: true },
 	compatibilityDate: '2024-04-03',
-	modules: ['@pinia/nuxt'],
-	// devtools: { enabled: true },
 });
