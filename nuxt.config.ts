1a999930a38c63d7de0dbcaece19a45353e6b4aa
spiderpig60 <spiderpig60@gmail.com>
2024-09-21 02:27:15 +0300

    drizzle installation & configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxthub/core','@pinia/nuxt'],
+	modules: ['@nuxthub/core', '@pinia/nuxt'],
 
 	hub: {
+		// https://hub.nuxt.com/docs/features/database, https://hub.nuxt.com/docs/recipes/drizzle
 		database: true,
 	},
 
