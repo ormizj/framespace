dd2892cb94849b3b212b3a8a28e9d62bdca60332
spiderpig60 <spiderpig60@gmail.com>
2024-09-21 11:02:05 +0300

    migration update

--- server/database/migrations/0000_broad_magik.sql
+++ server/database/migrations/0000_broad_magik.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `users` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`password` text NOT NULL,
 	`created_at` integer NOT NULL
