42d93ca9482daf96cf6c680d9b15d362cf61e054
spiderpig60 <spiderpig60@gmail.com>
2024-09-23 20:56:12 +0300

    auth wip

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,8 +1,15 @@
 import bcrypt from 'bcrypt';
 
 export default defineEventHandler(async (event) => {
-	const { login, password } = await readBody(event);
+	const { email, password } = await readBody(event);
 	const hashedPassword = ''; // TODO get password
+
+	if (password === '' || !/^[^@]+@[^@]/.test(email)) {
+		throw createError({
+			statusCode: 422,
+			statusMessage: 'Please provide a valid email and password.',
+		});
+	}
 
 	if (await bcrypt.compare(password, hashedPassword)) {
 	}
--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,13 +1,18 @@
 import bcrypt from 'bcrypt';
 
 export default defineEventHandler(async (event) => {
-	const { login, password } = await readBody(event);
+	const { email, password } = await readBody(event);
 	const hashedPassword = await bcrypt.hash(
 		password,
 		useRuntimeConfig(event).saltRounds
 	);
 
-	console.log(useRuntimeConfig(event).saltRounds);
+	if (password === '' || !/^[^@]+@[^@]/.test(email)) {
+		throw createError({
+			statusCode: 422,
+			statusMessage: 'Please provide a valid email and password.',
+		});
+	}
 
-	// TODO create account
+	return 'success';
 });
