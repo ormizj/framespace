49763af1477e5a47d157064fe32c25dad8cf7324
spiderpig60 <spiderpig60@gmail.com>
2024-12-05 00:22:47 +0200

    env fix

--- enums/OpenCorsSites.ts
+++ enums/OpenCorsSites.ts
@@ -1,6 +1,7 @@
 enum OpenCorsSites {
 	calculatorsoup = 'https://www.calculatorsoup.com/calculators/math/percentage.php',
 	framespace = 'http://localhost:3000/',
+	fitgirl = 'https://fitgirl-repacks.site/',
 }
 
 export default OpenCorsSites;
